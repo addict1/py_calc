c7b381d1b24264a964db6e3ebbf7686bc1f8247e
Tristan Charpentier <tristan_charpentier@hotmail.com>
2015-10-07 02:02:51 -0400

    Setup 2 and re-organized

---
+++ Unit_1/lesson_2.py
@@ -0,0 +1,27 @@
+def get_equation_from_points():
+    # get info
+    point1 = raw_input("Input X and Y separated by commas in (X,Y) >> ")
+    point2 = raw_input("Input X and Y separated by commas in (X,Y) >> ")
+    # organize info
+    point1 = point1.split(",")
+    point2 = point2.split(",")
+    x1 = float(point1[0])
+    y1 = float(point1[1])
+    x2 = float(point2[0])
+    y2 = float(point2[1])
+    # get slope
+    part1 = y2 - y1
+    part2 = x2 - x1
+    slope = part1 / part2
+    # get y-intercept
+    mx = slope * x1
+    if mx == 0:
+        b = y1
+    elif mx != 0:
+        b = y1 / mx
+    # print out final results
+    print "y = " + str(slope) + "x + " + str(b)
+
+def check_for_par():
+    # get info
+    equation1 = raw_input()
--- graph.py
+++ graph.py
@@ -12,13 +12,20 @@ if demand1 == "1":
     if demand2 == "1":
         print """[1] Get Midpoint from Equation
 [2] Solve Systems of Equations
-[3] Get Equation from Points
 """
         demand3 = raw_input("Pick number >> ")
         from Unit_1 import lesson_1
         if demand3 == "1":
             lesson_1.get_midpoint()
         elif demand3 == '2':
-            lesson1.solve_sys_of_equations()
-        elif demand3 == '3':
-            lesson1.get_equation_from_points()
+            lesson_1.solve_sys_of_equations()
+    if demmand2 == "2":
+        print """[1] Get Equation of Line from Points
+[2] Check for Parrallel
+[3] Check for Perpendicular"""
+        from Unit_1 import lesson_2
+        demand3 = raw_input("Pick number >> ")
+        if demmand3 == "1":
+            lesson_2.get_equation_from_points()
+        if demmand3 == "2":
+            lesson_2.check_for_par()
